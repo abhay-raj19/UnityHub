16161520e3cae852dc26ee94818888c429675c89
abhay-raj19 <rajpootabhay423@gmail.com>
2023-09-26 05:14:28 +0530

    Added up the fetching routes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -19,9 +19,49 @@ export const createPost = async (req, res) => {
     await newPost.save();
 
     const post = await Post.find();
-
     res.status(201).json(post);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
 };
+
+// READ 
+export const getFeedPosts = async(req,res) =>{
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+    
+  } catch (error) {
+    res.status(404).json({message:error.message});
+
+    
+  }
+}
+export const getUserPosts = async(req,res) =>{
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({userId})
+
+  } catch (error) {
+    res.status(404).json({message:error.message})
+  }
+}
+
+// UPDATE 
+export const likePost = async(req,res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if(isLiked) {
+      post.likes.delete(userId)
+    }
+
+
+    res.status(200).json()
+  } catch (error) {
+    res.status(404).json({message:error.message})
+  }
+}
