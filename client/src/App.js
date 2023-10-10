47747a4c2807a81dd0602f65c64494197d3be993
abhay-raj19 <rajpootabhay423@gmail.com>
2023-10-10 06:52:10 +0530

    feat:reducers and state maintainence

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import { BrowserRouter,Navigate,Routes,Route } from "react-router-dom";
+import ProfilePage from "scenes/ProfilePage";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+  return <div className="App">
+   <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<ProfilePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+    </Routes>
+   </BrowserRouter>
+  </div>
+
 }
 
 export default App;
