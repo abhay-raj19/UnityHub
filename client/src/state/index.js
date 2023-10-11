1c0ee213db13432df3e9720aa71cd51269f47ce6
abhay-raj19 <rajpootabhay423@gmail.com>
2023-10-11 06:43:58 +0530

    fix:initial-reducer states

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,3 +1,4 @@
+import { ExpandOutlined } from "@mui/icons-material";
 import { createSlice } from "@reduxjs/toolkit"; 
 
 const initialState ={
@@ -17,6 +18,30 @@ export const authSlice = createSlice({
         setLogin:(state,action) =>{
             state.user = action.payload.user;
             state.token = action.payload.token;
+        },
+        setLogout:(state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setFriends:(state,action) => {
+            if(state.user){
+                state.user.friends = action.payload.friends;
+            } else {
+                console.error("user friends non-existenr:(")
+            }
+        },
+        setPosts: (state,action) => {
+            state.posts = action.payload.posts;
+
+        },
+        setPost:(state,action) => {
+            const updatedPosts = state.posts.map((post) => {
+                if(post._id === action.payload.post_id) return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPosts;
         }
     }
 })
+export const { setMode,setLogin,setLogout,setFriends,setPosts,setPost} = authSlice.actions;
+export default authSlice.reducer;
